6a4d3581a9b564991f3cb1b9bf18981b553cc728
Neus Roman <neusroman@hotmail.com>
2023-10-05 13:06:21 +0200

    first commit

---
+++ resources/js/components/ChatMessages.vue
@@ -0,0 +1,21 @@
+<template>
+  <ul class="chat">
+    <li class="left clearfix" v-for="message in props.messages" :key="message.id">
+      <div class="clearfix">
+        <div class="header">
+          <strong>
+            {{ message.user.name }}
+          </strong>
+        </div>
+        <p>
+          {{ message.message }}
+        </p>
+      </div>
+    </li>
+  </ul>
+</template>
+<script setup>
+  const props = defineProps({
+    messages: Array
+  });
+</script>
