beef25d67ca76ce0b542530c08239fcad6941d7f
Neus Roman <neusroman@hotmail.com>
2024-01-07 18:02:38 +0100

    create message factory

---
+++ database/factories/MessageFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Message>
+ */
+class MessageFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::all()->random()->id,
+            'message' => Str::random(random_int(5, 15)),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+    }
+}
