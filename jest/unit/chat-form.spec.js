77aaed6f564def53d30774c21d98ff13ece47552
Neus Roman <neusroman@hotmail.com>
2024-01-08 17:14:07 +0100

    add chatform component tests

---
+++ jest/unit/chat-form.spec.js
@@ -0,0 +1,27 @@
+import { shallowMount } from "@vue/test-utils";
+import ChatForm from "../../resources/js/components/ChatForm.vue";
+
+describe('ChatForm.vue', () => {
+  it('component is rendered', () =>{
+    const wrapper = shallowMount(ChatForm);
+
+    expect(wrapper.find('#btn-input').exists());
+    expect(wrapper.find('#btn-chat').exists());
+  })
+  
+  it('send message event is emited with correct message', async () => {
+    const wrapper = shallowMount(ChatForm, {
+      props: {
+        user: 'TestUser',
+      },
+    });
+
+    await wrapper.find('#btn-input').setValue('Hello');
+
+    await wrapper.find('#btn-chat').trigger('click');
+
+    expect(wrapper.emitted('messagesent')).toBeTruthy();
+
+    expect(wrapper.emitted('messagesent')[0][0].message).toBe('Hello');
+  })
+})
